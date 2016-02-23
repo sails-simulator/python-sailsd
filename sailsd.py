f495a76bea11763e196117de92845d1c56cbb708
Louis Taylor <louis@kragniz.eu>
2016-02-23 10:58:05 +0000

    Add basic message sending

---
+++ sailsd.py
@@ -0,0 +1,24 @@
+import socket
+
+class Sailsd(object):
+    def _send_message_bytes(self, msg):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('localhost', 3333))
+
+        response = ''
+
+        try:
+            s.sendall(msg)
+
+            amount_received = 0
+
+            while amount_received < 1024:
+                data = s.recv(24)
+                amount_received += len(data)
+                response += data
+                if len(data) == 0:
+                    break
+        finally:
+            s.close()
+
+        return response
