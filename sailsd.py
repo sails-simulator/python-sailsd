61c5d1868505d0540cecf1218c10b24648b6cb2b
Louis Taylor <louis@kragniz.eu>
2016-03-13 10:28:35 +0000

    Add docstring to Sailsd

--- sailsd.py
+++ sailsd.py
@@ -2,6 +2,10 @@ import json
 import socket
 
 class Sailsd(object):
+    '''
+    Low-level control to the sailsd API. Not much is defined here, just direct
+    interaction to the API.
+    '''
     def _send_message_bytes(self, msg):
         #TODO assert that msg is bytes
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
